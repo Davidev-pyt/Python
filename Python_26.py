f0ca08e8a533c59f31eae7bd69792e9070eefda7
Davidev-pyt <serranojoseivan4@gmail.com>
2026-03-27 12:21:27 -0300

    Commit

---
+++ Python_26.py
@@ -0,0 +1,7 @@
+estado = dict()
+brasil = list()
+for c in range(3):
+    estado['uf'] = str(input('Unidade Federativa: '))
+    estado['sigla'] = str(input('Sigla do estado: '))
+    brasil.append(estado.copy())
+print(brasil)
---
+++ Python_28.py
@@ -0,0 +1,20 @@
+from random import randint
+from time import sleep
+from operator import itemgetter
+dado = {'jogador1': randint(1, 6),
+        'jogador2': randint(1, 6),
+        'jogador3': randint(1, 6),
+        'jogador4': randint(1, 6)}
+ranking = ()
+
+print('Valores Sorteados')
+
+for k , v in dado.items():
+    print(f'{k} tirou {v} no dado.')
+    sleep(1)
+ranking = sorted(dado.items(), key= itemgetter(1), reverse=True)
+print('-=' * 25)
+print(' === Ranking dos Jogadores ===')
+for i , v in enumerate(ranking):
+    print(f'{i+1}º lugar: {v[0]} com {v[1]}')
+    sleep(1)
