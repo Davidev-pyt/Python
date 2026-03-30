498afb4eff631a5dd89436d29c66325b17ee0039
Davidev-pyt <serranojoseivan4@gmail.com>
2026-03-30 10:23:30 -0300

    Commit

--- Python_31.py
+++ Python_31.py
@@ -1,11 +1,26 @@
-def escreva(msg):
-    tam = len(msg)
-    print('~' * tam)
-    print(msg)
-    print('~' * tam)
+from time import sleep
+def maior(* num):
+    cont = maior = 0
+    print('\nAnalisando os valores passados...')
+    for valor in num:
+        print(f'{valor}',end=' ')
+        sleep(0.5)
+
+        if cont == max:
+            maior = valor
+            cont += 1
+        else:
+            if valor > maior:
+                maior = valor
+                cont += 1
+    print(f'Foram informados {len(num)} valores ao todo.')
+    print(f'O maior valor informado foi {maior}.')
 
 
 
 
-escreva('Davi Henrique')
-escreva('Ola')
+maior(2, 9, 4, 5, 7, 1)
+maior(4, 7, 0)
+maior(1, 2)
+maior(6)
+maior(0)
