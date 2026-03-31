8ee6129743e33c3cbc49423eebcbe449ddf6f360
Davidev-pyt <serranojoseivan4@gmail.com>
2026-03-31 12:19:52 -0300

    Commit

--- Python_33.py
+++ Python_33.py
@@ -1,23 +1,12 @@
-def fatorial(num=1, show=False):
-    """Calcula o fatorial de um número.
-    :param num: O número a ser calculado o fatorial (padrão é 1).
-    :param show: (opcional) Se True, mostra a conta."""
-    f = 1
-    for c in range(num, 0, -1):
-        if show:
-            print(f'{c}', end=' ')
-            if c > 1:
-                print('x', end=' ')
-            else:
-                print('=', end=' ')
-        f *= c
-        return f 
-
-
-
-
-
-
-
-
-print(fatorial(5, show=True))
+def ficha(jog='<desconhecido>' , gol=0):
+    print(f'O {jog} fez {gol} gol(s) no campeonato.')
+nome = str(input('Nome do jogador: '))
+gols = str(input('Número de gols: '))
+if gols.isnumeric():
+        gols = int(gols)
+else:
+    gols = 0
+if nome.strip() == '':
+        ficha(gol=gols)
+else:
+        ficha(nome , gols)
